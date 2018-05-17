0ddb8473043cb4b134adae2543c21f7c54b5eb3b
vatche-chamlian <vatche.chamlian@penzias.com>
2018-05-17 00:12:49 -0400

    Added json parsing method to js and modified styles and markup to handle it

--- theme/js/edScript.js
+++ theme/js/edScript.js
@@ -1,13 +1,70 @@
 /* because you never know when you might need some jquery */
 $(document).ready(function(){
-  // this can be real later
-  var ajaxResults = true;
+  // hide the battle-box result display
+  $('.battle-box--skew').hide();
 
-  // if we get results do something fancy
-  if (ajaxResults) {
-    $('form h2 span').text('Another');
-    typeMe('.typewriter p');
-  }
+  // testing purposes only
+  // $('.battle-box--skew').show();
+  // var obj = {"ship": "Cobra MkIII", "station": "Morgue's Mortuary", "days": 7, "hours": 7.686388888888889, "commander": "kriation"};
+  // console.log(obj);
+
+  // var ship = obj['ship']
+  //     station = obj['station']
+  //     commander = obj['commander']
+  //     days = obj['days']
+  //     hours = obj['hours'];
+
+  // var simpleHours = ~~hours
+  //     time = days + ' days and ' + simpleHours + ' hours ago';
+
+  // $('.battle-box--skew').append('<p><strong>Ship: </strong>' + ship + '</p> <p><strong>Location: </strong>' + station + '</p><p><strong>Reported: </strong>' + time + '</p>');
+
+  $('button.target-commander').click(function(e){
+    //grab the commander's name from the input field
+    var commanderName = $('input.rq-form-element').val();
+
+      // let there be ajax!
+      $.ajax({
+        type: 'GET',
+        url: 'https://ivzn3c5vf1.execute-api.us-east-1.amazonaws.com/live/cmdr-info',
+        data: { 
+            commander: commanderName,
+        },
+        success: function(result) {
+          obj = JSON.parse(result.responseText);
+
+          var ship = obj['ship']
+              station = obj['station']
+              commander = obj['commander']
+              days = obj['days']
+              hours = obj['hours']
+              // a lil love for those numbahs
+              simpleHours = ~~hours
+              time = days + ' days and ' + simpleHours + ' hours ago';
+
+          $('.battle-box--skew').append('<p><strong>Ship: </strong>' + ship + '</p> <p><strong>Location: </strong>' + station + '</p><p><strong>Reported: </strong>' + time + '</p>');
+          // if there is a successful return of data add "Another" to the form text
+          $('form h2 span').text('Another');
+          // show the battle box result before typewriter effect
+          $('.battle-box--skew').show();
+          // typewriter effect engaged
+          typeMe('.typewriter p');
+        },
+        error: function(result) {
+          console.log(result);
+          // add error classes and change text
+          $('.battle-box--skew').addClass('error');
+          $('.typewriter h3').addClass('error').text('Commander not found');
+          $('.typewriter p:first').addClass('error').text('Or there was an error');
+          // show the battle box result before typewriter effect
+          $('.battle-box--skew').show();
+          typeMe('.typewriter p:first');
+        }
+      });
+      // expected json 
+      e.preventDefault();
+  });
+  // expected json response: {"ship": "Cobra MkIII", "station": "Morgue's Mortuary", "days": 7, "hours": 7.686388888888889, "commander": "kriation"}
 
   // the fancy shmancy mentioned above
   function typeMe(el) {
